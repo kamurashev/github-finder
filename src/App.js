6a732723b757bd896285356bb541f3fadee61f93
kamurashev <kamurashev@gmail.com>
2021-07-27 23:46:44 +0300

    - added default (not found) page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
 import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:username' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
