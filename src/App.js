6648a5e72cc22f5adcf9067758728a52d18a141b
kamurashev <kamurashev@gmail.com>
2021-09-01 05:07:13 +0300

    -prepare for deploy

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <GithubState>
       <AlertState>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <div className='App'>
             <Navbar />
             <div className='container'>
